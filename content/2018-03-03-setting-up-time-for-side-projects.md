a223c693823d1ed41d1d9e77d9a1c09f4117d327
AlahmadiQ8 <m91.alahmadi@gmail.com>
2018-03-19 21:49:51 -0500

    added deploy script

--- content/2018-03-03-setting-up-time-for-side-projects.md
+++
@@ -1,7 +0,0 @@
----
-title: YAAASSSS
-date: 2018-03-04T13:44:26-08:00
-draft: false
----
-
-OMG OMG This is my page
---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BUILD_DIR=public
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+echo "push gh-pages branch"
+git push origin gh-pages
+
+cd ..
